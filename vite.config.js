51a1fff1dbc3215235b82caefeaff5931884c8fe
Daniel Jussef <jussef.daniel@gmail.com>
2025-12-04 10:44:04 -0600

    workflows build vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
+  base: '/calendario-adviento-2025/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
